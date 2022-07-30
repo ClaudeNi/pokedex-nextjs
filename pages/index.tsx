610ca95a76dc87a632eb8140edff1ea53f29fdb1
ClaudeNi <claudenij2@gmail.com>
2022-07-30 23:35:12 +0300

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      test
+    <div className={styles.main}>
+      <h1>Pokemon Searcher</h1>
     </div>
   )
 }
