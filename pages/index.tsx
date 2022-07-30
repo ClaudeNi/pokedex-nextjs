b25dca96c61c670e523fe88949777d4a60f79c2d
ClaudeNi <claudenij2@gmail.com>
2022-07-31 00:15:11 +0300

    started showing the first 20 pokemon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,34 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+const Home: NextPage = (props:any) => {
   return (
     <div className={styles.container}>
-      Pokemon
+      <h1>Pokedex</h1>
+      <ul>
+        {props.pokemonInfo.map((pokemon:any, id:number) => {
+          return <li key={id + 1}>{pokemon.name}</li>
+        })}
+      </ul>
     </div>
+    
   )
+}
+
+export async function getStaticProps(context:any) {
+  try {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20`)
+    const { results } = await res.json()
+    const pokemonInfo = results.map((pokemon:String, index:number) => {
+      const id:number = index + 1
+      return {...pokemon}
+    })
+    return {
+      props: { pokemonInfo }
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export default Home
