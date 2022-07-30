f55d2eec19332beed080d4269605136938c47d64
ClaudeNi <claudenij2@gmail.com>
2022-07-30 23:49:03 +0300

    new nextjs files

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.main}>
-      <h1>Pokemon Searcher</h1>
+    <div className={styles.container}>
+      Pokemon
     </div>
   )
 }
