a695b53f2de3962ba798b94e97b005a9ab421057
ClaudeNi <claudenij2@gmail.com>
2022-08-12 23:57:34 +0300

    added id

--- pages/pokemon.tsx
+++ pages/pokemon.tsx
@@ -4,17 +4,19 @@ import styles from '../styles/Pokemon.module.css'
 export default function pokemon({ pokemon }:any) {
     const showTypes = () => {
         return pokemon.types.map((type:any,i:number) => {
-            return <li key={i}>{type.type.name}</li>
+            return <li key={i} className={styles[type.type.name]}>{type.type.name}</li>
         })
     }
     
     return <div className={styles.pokePage}>
         <div className={`${styles.upperHalf} ${styles[pokemon.types[0].type.name]}`}>
+            <span className={styles.pokeID}>#{pokemon.id}</span>
             <span className={styles.pokeName}>{pokemon.name}</span>
             <img src={pokemon.image} alt={pokemon.name} className={styles.pokeImg}/>
         </div>
         <div className={styles.lowerHalf}>
             <ul>
+                <li>Types:</li>
                 {showTypes()}
             </ul>
         </div>
